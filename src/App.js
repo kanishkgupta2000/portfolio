cefe03abedeca9a9d7bada5e8aa1e60d2ed62afc
Kanishk Gupta <kanishkgupta2000@gmail.com>
2019-08-18 16:10:09 +0530

    setup page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Index from './components/Index';
+import About from './components/About';
+import Contact from './components/Contact';
+import Work from './components/Work';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<Router>
+    <Fragment>
+    <Header/>
+		<Switch>
+		<Route exact path='/' component={Index}/>
+		<Route exact path='/home' component={Index}/>
+		<Route exact path='/about' component={About}/>
+		<Route exact path='/contact' component={Contact}/>
+    <Route exact path='/work' component={Work}/>
+		</Switch>
+		
+    <Footer/>
+	</Fragment>
+	</Router>
+
+
+  )
 }
 
-export default App;
+export default App
